f29d3424190992a0f8b787b9ecaf792f4cd30f5f
Joseph Eaton <josepheaton04@outlook.com>
2025-02-15 18:11:24 +0000

    style header and footer more closely to notepad

--- scripts.js
+++ scripts.js
@@ -2,14 +2,14 @@ const btn = document.getElementById("btn-toggle-theme");
 
 btn.addEventListener("click", (e) => {
 	var root = document.documentElement;
-	if (root.getAttribute('data-theme') !== "dark")
+	if (root.getAttribute('theme') !== "dark")
 	{
-		root.setAttribute("data-theme", "dark");
+		root.setAttribute("theme", "dark");
 		console.log("To dark mode!");
 	}
 	else
 	{
-		root.setAttribute("data-theme", "light");
+		root.setAttribute("theme", "light");
 		console.log("To light mode!");
 	}
 });
